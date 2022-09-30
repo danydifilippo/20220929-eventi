4d80922d3411fa1866e50aad11ef0f55ad7f809f
danydifilippo <danydifilippo@gmail.com>
2022-09-30 08:51:18 +0200

    final

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ function font(stile) {
     blog.classList.toggle(stile)
 }
 
+let todolist = document.getElementById(todo)
+
+function input(eventObject) {
+    if (eventObject.key !== 'Enter') {
+        return;
+    }
+    let input = document.getElementById('inputTodo')
+    if (input.value !=='') {
+        todolist.innerHTML += `<div class="note">${input.value}</div>`
+    }
+    input.value =''
+}
+
